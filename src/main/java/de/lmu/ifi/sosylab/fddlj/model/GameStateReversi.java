25d166eff2b99042c5b92e8e74ad19b4bff6289a
Leidreiter <leidreiter@cip.ifi.lmu.de>
2019-12-16 20:24:18 +0100

    Placeholder Implementation of GameState
    Placeholder implementation to allow for unit testing.

---
+++ src/main/java/de/lmu/ifi/sosylab/fddlj/model/GameStateReversi.java
@@ -0,0 +1,50 @@
+package de.lmu.ifi.sosylab.fddlj.model;
+
+public class GameStateReversi implements GameState, ModifiableGameState {
+
+  public GameStateReversi() {
+    // Initiate GameState
+  }
+
+  @Override
+  public void setCurrentPhase(Phase phase) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void setGameField(GameField gameField) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void setPlayerManagement(PlayerManagement playerManagement) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public Phase getCurrentPhase() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public GameField getField() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public PlayerManagement getPlayerManagement() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public GameState makeCopy() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
