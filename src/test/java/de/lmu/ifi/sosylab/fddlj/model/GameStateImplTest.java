c38d8c28e3864f0e8d8d8e0dbb40c0b4c4782279
Leidreiter <leidreiter@cip.ifi.lmu.de>
2019-12-17 13:42:24 +0100

    Improved unit tests for GameStateImpl
    Mostly finished up the unit tests. Still awaiting some dependencies.

--- src/test/java/de/lmu/ifi/sosylab/fddlj/model/GameStateImplTest.java
+++ src/test/java/de/lmu/ifi/sosylab/fddlj/model/GameStateImplTest.java
@@ -16,67 +16,163 @@ public class GameStateImplTest {
   @Test
   void testSetAndGetPhase_RUNNING() {
     state.setCurrentPhase(Phase.RUNNING);
-    Assertions.assertEquals(Phase.RUNNING, state.getCurrentPhase());
+    Assertions.assertEquals(
+        Phase.RUNNING, state.getCurrentPhase(), "Phase unsuccessfully set to RUNNING!");
   }
 
   @Test
   void testSetAndGetPhase_FINISHED() {
     state.setCurrentPhase(Phase.FINISHED);
-    Assertions.assertEquals(Phase.FINISHED, state.getCurrentPhase());
+    Assertions.assertEquals(
+        Phase.FINISHED, state.getCurrentPhase(), "Phase unsuccessfully set to FINISHED!");
   }
 
   @Test
   void testSetAndGetPhase_WAITING() {
     state.setCurrentPhase(Phase.WAITING);
-    Assertions.assertEquals(Phase.WAITING, state.getCurrentPhase());
+    Assertions.assertEquals(
+        Phase.WAITING, state.getCurrentPhase(), "Phase unsuccessfully set to WAITING!");
   }
-  
+
   @Test
   void testSetAndGetPhase_null() {
     state.setCurrentPhase(null);
-    Assertions.assertEquals(null, state.getCurrentPhase());
+    Assertions.assertEquals(null, state.getCurrentPhase(), "Phase unsuccessfully set to null!");
   }
 
   @Test
   void testSetAndGetGameField_notNull() {
     GameField field;
+    // set up field
     state.setGameField(field);
-    Assertions.assertEquals(field, state.getField());
+    Assertions.assertSame(
+        field,
+        state.getField(),
+        "getField should return the same GameField that was set by setGameField!");
   }
 
   @Test
   void testSetAndGetGameField_null() {
-    GameField field = null;
-    state.setGameField(field);
-    Assertions.assertEquals(null, state.getField());
+    state.setGameField(null);
+    Assertions.assertSame(null, state.getField(), "GameField unsuccessfully set to null!");
   }
 
   @Test
   void testSetAndGetPlayerManagement_notNull() {
     PlayerManagement manager;
+    // set up manager
     state.setPlayerManagement(manager);
-    Assertions.assertEquals(manager, state.getPlayerManagement());
+    Assertions.assertSame(
+        manager,
+        state.getPlayerManagement(),
+        "getPlayerManagement should return the same PlayerManagement that was set by setPlayerManagement!");
   }
 
   @Test
   void testSetAndGetPlayerManagement_null() {
-    PlayerManagement manager = null;
-    state.setPlayerManagement(manager);
-    Assertions.assertEquals(null, state.getPlayerManagement());
+    state.setPlayerManagement(null);
+    Assertions.assertSame(
+        null, state.getPlayerManagement(), "PlayerManagement unsuccessfully set to null!");
   }
 
   @Test
-  void testMakeCopy_notNull() {}
+  void testMakeCopy_notNull() {
+    Phase phase = Phase.RUNNING;
+    GameField field;
+    // set up field
+    PlayerManagement manager;
+    // set up manager
+    state.setCurrentPhase(phase);
+    state.setGameField(field);
+    state.setPlayerManagement(manager);
+
+    GameState stateCopy;
+    stateCopy = state.makeCopy();
+
+    Assertions.assertNotSame(
+        field, stateCopy.getField(), "GameField was not deep copied by makeCopy!");
+    Assertions.assertNotSame(
+        manager,
+        stateCopy.getPlayerManagement(),
+        "PlayerManagement was not deep copied by makeCopy!");
+
+    Assertions.assertEquals(
+        phase, stateCopy.getCurrentPhase(), "makeCopy should have copied Phase!");
+    Assertions.assertEquals(field, stateCopy.getField(), "makeCopy should have copied GameField!");
+    Assertions.assertEquals(
+        manager, stateCopy.getPlayerManagement(), "makeCopy should have copied PlayerManagement!");
+  }
 
   @Test
-  void testMakeCopy_PhaseNull() {}
+  void testMakeCopy_PhaseNull() {
+    GameField field;
+    // set up field
+    PlayerManagement manager;
+    // set up manager
+    state.setCurrentPhase(null);
+    state.setGameField(field);
+    state.setPlayerManagement(manager);
+
+    GameState stateCopy;
+    stateCopy = state.makeCopy();
+
+    Assertions.assertNotSame(
+        field, stateCopy.getField(), "GameField was not deep copied by makeCopy!");
+    Assertions.assertNotSame(
+        manager,
+        stateCopy.getPlayerManagement(),
+        "PlayerManagement was not deep copied by makeCopy!");
+
+    Assertions.assertEquals(
+        null, stateCopy.getCurrentPhase(), "makeCopy should have copied Phase!");
+    Assertions.assertEquals(field, stateCopy.getField(), "makeCopy should have copied GameField!");
+    Assertions.assertEquals(
+        manager, stateCopy.getPlayerManagement(), "makeCopy should have copied PlayerManagement!");
+  }
 
   @Test
-  void testMakeCopy_GameFieldNull() {}
+  void testMakeCopy_GameFieldNull() {
+    Phase phase = Phase.WAITING;
+    PlayerManagement manager;
+    // set up manager
+    state.setCurrentPhase(phase);
+    state.setGameField(null);
+    state.setPlayerManagement(manager);
+
+    GameState stateCopy;
+    stateCopy = state.makeCopy();
+
+    Assertions.assertNotSame(
+        manager,
+        stateCopy.getPlayerManagement(),
+        "PlayerManagement was not deep copied by makeCopy!");
+
+    Assertions.assertEquals(
+        phase, stateCopy.getCurrentPhase(), "makeCopy should have copied Phase!");
+    Assertions.assertEquals(null, stateCopy.getField(), "makeCopy should have copied GameField!");
+    Assertions.assertEquals(
+        manager, stateCopy.getPlayerManagement(), "makeCopy should have copied PlayerManagement!");
+  }
 
   @Test
-  void testMakeCopy_PlayerManagementNull() {}
+  void testMakeCopy_PlayerManagementNull() {
+    Phase phase = Phase.FINISHED;
+    GameField field;
+    // set up field
+    state.setCurrentPhase(phase);
+    state.setGameField(field);
+    state.setPlayerManagement(null);
 
-  @Test
-  void testMakeCopy_stateNull() {}
+    GameState stateCopy;
+    stateCopy = state.makeCopy();
+
+    Assertions.assertNotSame(
+        field, stateCopy.getField(), "GameField was not deep copied by makeCopy!");
+
+    Assertions.assertEquals(
+        phase, stateCopy.getCurrentPhase(), "makeCopy should have copied Phase!");
+    Assertions.assertEquals(field, stateCopy.getField(), "makeCopy should have copied GameField!");
+    Assertions.assertEquals(
+        null, stateCopy.getPlayerManagement(), "makeCopy should have copied PlayerManagement!");
+  }
 }
