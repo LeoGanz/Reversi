fcaa163e309f932010146abcb14db7a96ecd1101
Leidreiter <leidreiter@cip.ifi.lmu.de>
2019-12-20 13:28:38 +0100

    Dummy Implementation for Teamwork
    Just implemented method headers and GameMode Enum

---
+++ src/main/java/de/lmu/ifi/sosylab/fddlj/model/GameMode.java
@@ -0,0 +1,5 @@
+package de.lmu.ifi.sosylab.fddlj.model;
+
+public enum GameMode {
+  SINGLEPLAYER, HOTSEAT, MULTIPLAYER
+}
---
+++ src/main/java/de/lmu/ifi/sosylab/fddlj/model/ModelImpl.java
@@ -0,0 +1,60 @@
+package de.lmu.ifi.sosylab.fddlj.model;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Set;
+
+import chess.model.GameState;
+
+public class ModelImpl implements Model{
+
+  private final PropertyChangeSupport support;
+  
+  private ModifiableGameState state;
+  private GameMode mode;
+  //private AI ai;
+  
+  // HotSeat and Single
+  public ModelImpl(GameMode mode) {}
+  
+  //Multiplayer
+  public ModelImpl(Player thisClient, PlayerManagement manager) {}
+  
+  @Override
+  public boolean placeDisk(Disk disk, Cell cell) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Set<Cell> getPossibleMovesForPlayer(Player player) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void addListener(PropertyChangeListener pcl) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void removeListener(PropertyChangeListener pcl) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public GameState getState() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+  private boolean checkIfLegalMove(Disk disk, Cell cell) {
+    return false;
+  }
+  
+  private void notifyListeners() {
+  }
+
+}
