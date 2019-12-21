089ec96ce8da846e17066c343bb121644c52fdf8
Leidreiter <leidreiter@cip.ifi.lmu.de>
2019-12-21 15:12:02 +0100

    Mostly finished ModelImpl and added JavaDoc

--- src/main/java/de/lmu/ifi/sosylab/fddlj/model/ModelImpl.java
+++ src/main/java/de/lmu/ifi/sosylab/fddlj/model/ModelImpl.java
@@ -1,11 +1,18 @@
 package de.lmu.ifi.sosylab.fddlj.model;
 
+import static java.util.Objects.requireNonNull;
+
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
-/** @author Daniel Leidreidter, Dora Pruteanu */
+/**
+ * A concrete Implementation of the {@link Model} interface.
+ *
+ * @author Daniel Leidreiter, Dora Pruteanu
+ */
 public class ModelImpl implements Model {
 
   private final PropertyChangeSupport support;
@@ -17,57 +24,81 @@ public class ModelImpl implements Model {
   private int numberOfPlayerOneDisks = DISKS_PER_PLAYER;
   private int numberOfPlayerTwoDisks = DISKS_PER_PLAYER;
 
-  // HotSeat and Single
-  public ModelImpl(GameMode mode, PlayerManagement manager) {
+  /**
+   * Creates a new game with a given {@link GameMode} and two given {@link Player}.
+   *
+   * @param mode the game shall be played in.
+   * @param playerOne who will play in this game.
+   * @param playerTwo who will play in this game.
+   */
+  public ModelImpl(GameMode mode, Player playerOne, Player playerTwo) {
+    support = new PropertyChangeSupport(this);
+    this.mode = mode;
     state = new GameStateImpl();
     state.setGameField(new GameFieldImpl());
     state.setCurrentPhase(Phase.RUNNING);
-    this.mode = mode;
+    ModifiablePlayerManagement manager = new PlayerManagementImpl(playerOne, playerTwo);
     state.setPlayerManagement(manager);
+    if (mode.equals(GameMode.SINGLEPLAYER)) {
+      // TODO Initiate AI
+    }
   }
 
-  /*// Multiplayer
-  public ModelImpl(Player thisClient, PlayerManagement manager) {
-    state = new GameStateImpl();
-    state.setPlayerManagement(manager);
-    if (manager.getCurrentPlayer() == thisClient) {
-      state.setCurrentPhase(Phase.WAITING);
-    } else {
-      state.setCurrentPhase(Phase.RUNNING);
-    }
-    state.setGameField(new GameFieldImpl());
-    this.mode = GameMode.MULTIPLAYER;
-  }*/
+  /**
+   * Creates a new game with a deep copy of the given {@link GameState}.
+   *
+   * @param newState with which the game shall be initialized.
+   */
+  ModelImpl(GameState newState) {
+    support = new PropertyChangeSupport(this);
+    state = (ModifiableGameState) newState.makeCopy();
+    this.mode = GameMode.HOTSEAT;
+  }
 
   @Override
   public boolean placeDisk(Disk disk, Cell cell) {
     if (!state.getCurrentPhase().equals(Phase.RUNNING)) {
       return false;
     }
-    if ((state.getPlayerManagement().getCurrentPlayer()
-            == state.getPlayerManagement().getPlayerOne())
-        && numberOfPlayerOneDisks <= 0) {
-      return false;
-    }
-    if ((state.getPlayerManagement().getCurrentPlayer()
-            == state.getPlayerManagement().getPlayerTwo())
-        && numberOfPlayerTwoDisks <= 0) {
-      return false;
-    }
+
     if (checkIfLegalMove(disk, cell)) {
       ModifiableGameField field = (ModifiableGameField) state.getField();
       field.set(cell, disk);
-      /*changeDisks();*/
+      turnDisks(cell);
+
       if (state.getPlayerManagement().getCurrentPlayer()
           == state.getPlayerManagement().getPlayerOne()) {
         numberOfPlayerOneDisks--;
+        if (numberOfPlayerTwoDisks <= 0) {
+          state.setCurrentPhase(Phase.FINISHED);
+          handleWinner();
+        }
       }
       if (state.getPlayerManagement().getCurrentPlayer()
           == state.getPlayerManagement().getPlayerTwo()) {
         numberOfPlayerTwoDisks--;
-      } /*
-        checkIfGameIsFinished();
-        changePlayer();*/
+        if (numberOfPlayerOneDisks <= 0) {
+          state.setCurrentPhase(Phase.FINISHED);
+          handleWinner();
+        }
+      }
+
+      if (state.getCurrentPhase().equals(Phase.RUNNING)) {
+        ModifiablePlayerManagement manager =
+            (ModifiablePlayerManagement) state.getPlayerManagement();
+        manager.switchCurrentPlayer();
+      }
+
+      notifyListeners();
+
+      if (mode.equals(GameMode.SINGLEPLAYER)) {
+        // TODO let AI move if SinglePlayer
+        // TODO notify Listeners
+      }
+      return true;
+
+    } else {
+      return false;
     }
   }
 
@@ -77,7 +108,7 @@ public class ModelImpl implements Model {
     // TODO Change 8 to GameFieldImpl.SIZE
     for (int column = 0; column < 8; column++) {
       for (int row = 0; row < 8; row++) {
-        Cell checkedCell = new CellImpl();
+        Cell checkedCell = new CellImpl(column, row);
         Disk disk = new DiskImpl(state.getPlayerManagement().getCurrentPlayer());
         if (checkIfLegalMove(disk, checkedCell)) {
           listOfMoves.add(checkedCell);
@@ -89,14 +120,14 @@ public class ModelImpl implements Model {
 
   @Override
   public void addListener(PropertyChangeListener pcl) {
-    // TODO Auto-generated method stub
-
+    requireNonNull(pcl);
+    support.addPropertyChangeListener(pcl);
   }
 
   @Override
   public void removeListener(PropertyChangeListener pcl) {
-    // TODO Auto-generated method stub
-
+    requireNonNull(pcl);
+    support.removePropertyChangeListener(pcl);
   }
 
   @Override
@@ -104,6 +135,63 @@ public class ModelImpl implements Model {
     return state;
   }
 
+  // TODO add method to interface to set the Model to Waiting if running
+
+  /**
+   * Turns all opponent's Disks lying between the placed Disk and another Disk of the same Player.
+   *
+   * @param cell at which a disk was recently placed. The cell must not be empty.
+   */
+  private void turnDisks(Cell cell) {
+
+    assert state.getField().get(cell).isPresent();
+    Player player = state.getField().get(cell).get().getPlayer();
+
+    Player opponentPlayer =
+        (player == state.getPlayerManagement().getPlayerOne())
+            ? state.getPlayerManagement().getPlayerTwo()
+            : state.getPlayerManagement().getPlayerOne();
+
+    for (int x = -1; x < 2; x++) {
+      for (int y = -1; y < 2; y++) {
+        if (!(x == 0 && y == 0)) {
+          Cell cellChecked = new CellImpl(cell.getColumn() + x, cell.getRow() + y);
+          if (state.getField().isWithinBounds(cellChecked)
+              && (state.getField().isCellOfPlayer(opponentPlayer, cellChecked))) {
+            Set<Cell> toTurn = new HashSet<>();
+            int xcoor = cellChecked.getColumn();
+            int ycoor = cellChecked.getRow();
+            Cell opponentCell = new CellImpl(xcoor, ycoor);
+
+            while (state.getField().isWithinBounds(opponentCell)
+                && (state.getField().isCellOfPlayer(opponentPlayer, opponentCell))) {
+              toTurn.add(opponentCell);
+              xcoor = xcoor + x;
+              ycoor = ycoor + y;
+              opponentCell = new CellImpl(xcoor, ycoor);
+            }
+
+            if (state.getField().isWithinBounds(opponentCell)
+                && state.getField().isCellOfPlayer(player, opponentCell)) {
+              for (Cell c : toTurn) {
+                ModifiableGameField field = (ModifiableGameField) state.getField();
+                field.set(c, new DiskImpl(player));
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Checks whether a given {@link Disk} can be placed at a given {@link Cell} according to the
+   * Reversi rules.
+   *
+   * @param disk which is tested for whether it can be placed.
+   * @param cell which is tested for whether the disk can be placed here.
+   * @return {@code true} if the disk can be placed according to the rules, {@code false} otherwise.
+   */
   private boolean checkIfLegalMove(Disk disk, Cell cell) {
 
     if (!(disk.getPlayer() == state.getPlayerManagement().getCurrentPlayer())) {
@@ -149,8 +237,18 @@ public class ModelImpl implements Model {
     return false;
   }
 
-  private void notifyListeners() {}
+  /** Notifies all Listeners of a changed {@link GameState}. */
+  private void notifyListeners() {
+    support.firePropertyChange("State changed", null, state.makeCopy());
+  }
 
+  /**
+   * Checks whether a disk can be placed at the given {@link Cell} during the first four moves.
+   *
+   * @param cell which shall be checked.
+   * @return {@code true} if the disk can be placed at the cell during the first four moves, {@code
+   *     false} otherwise.
+   */
   private boolean beginningMove(Cell cell) {
     if (state.getField().getCellsOccupiedWithDisks().size() < 5) {
       if (cell.getRow() > 2
@@ -162,5 +260,24 @@ public class ModelImpl implements Model {
       }
     }
     return false;
+  }
+
+  /** Checks who has won if the game is finished and sets the winner. */
+  private void handleWinner() {
+    if (!(state.getCurrentPhase().equals(Phase.FINISHED))) {
+      return;
+    }
+    int numberOfDiscsPlayerOne =
+        state.getField().getAllCellsForPlayer(state.getPlayerManagement().getPlayerOne()).size();
+    int numberOfDiscsPlayerTwo =
+        state.getField().getAllCellsForPlayer(state.getPlayerManagement().getPlayerTwo()).size();
+    ModifiablePlayerManagement manager = (ModifiablePlayerManagement) state.getPlayerManagement();
+
+    if (numberOfDiscsPlayerOne > numberOfDiscsPlayerTwo) {
+      manager.setWinner(Optional.of(manager.getPlayerOne()));
+    }
+    if (numberOfDiscsPlayerOne < numberOfDiscsPlayerTwo) {
+      manager.setWinner(Optional.of(manager.getPlayerTwo()));
+    }
   }
 }
