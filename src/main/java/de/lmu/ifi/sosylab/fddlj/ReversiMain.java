609f6a262f2d1378d2da38fcdd1d1ca5ac5f35aa
Josef Feger <J.Feger@campus.lmu.de>
2019-12-22 12:21:29 +0100

    Implemented main class and set gradle build path.

---
+++ src/main/java/de/lmu/ifi/sosylab/fddlj/ReversiMain.java
@@ -0,0 +1,29 @@
+package de.lmu.ifi.sosylab.fddlj;
+
+/**
+ * Main class of the reversi application.
+ * 
+ * @author Josef Feger.
+ */
+public class ReversiMain {
+
+  /**
+   * Invoke the actual starting method {@link ChessMain#showPawnGame()}.
+   *
+   * @param args The command line arguments.
+   */
+  public static void main(String[] args) {
+    ReversiMain.showGame(args);
+
+  }
+
+  /**
+   * Parse input arguments and start the game accordingly.
+   * 
+   * @param args the input arguments to parse
+   */
+  private static void showGame(String[] args) {
+    // TODO initialize Controller and call start screen
+  }
+
+}
