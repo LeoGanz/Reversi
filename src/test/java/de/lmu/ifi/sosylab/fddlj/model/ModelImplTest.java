2848d2562bee712b06427943424f5832070ff97c
Dora Pruteanu <dora@Dragoss-MBP.fritz.box>
2020-01-02 10:51:17 +0100

    Added ModelImplTest.

--- src/test/java/de/lmu/ifi/sosylab/fddlj/model/ModelImplTest.java
+++ src/test/java/de/lmu/ifi/sosylab/fddlj/model/ModelImplTest.java
@@ -1,5 +1,6 @@
 package de.lmu.ifi.sosylab.fddlj.model;
 
+import javafx.scene.paint.Color;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.util.Map;
@@ -8,6 +9,23 @@ import java.util.Set;
 
 public class ModelImplTest {
 
+  private ModifiableGameState earlyGame_PlayerTwosTurn() {
+    Player one = new PlayerImpl("Tina", Color.ANTIQUEWHITE);
+    Player two = new PlayerImpl("Rhea", Color.ALICEBLUE);
+    ModifiablePlayerManagement manager = new PlayerManagementImpl(one, two);
+    ModifiableGameField field = new GameFieldImpl();
+    field.set(new CellImpl(3, 3), new DiskImpl(manager.getPlayerOne()));
+    field.set(new CellImpl(3, 4), new DiskImpl(manager.getPlayerOne()));
+    field.set(new CellImpl(3, 5), new DiskImpl(manager.getPlayerOne()));
+    field.set(new CellImpl(4, 3), new DiskImpl(manager.getPlayerTwo()));
+    field.set(new CellImpl(4, 4), new DiskImpl(manager.getPlayerTwo()));
+    field.set(new CellImpl(4, 5), new DiskImpl(manager.getPlayerTwo()));
 
+    manager.switchCurrentPlayer();
 
+    ModifiableGameState state = new GameStateImpl();
+    state.setGameField(field);
+    state.setCurrentPhase(Phase.RUNNING);
+    return state;
+  }
 }
