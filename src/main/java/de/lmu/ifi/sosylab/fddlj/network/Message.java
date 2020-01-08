b23d1d2c8976fd28027e50513fd6988ad7c2a24d
Florian Theimer <f.theimer@xolf.info>
2020-01-09 00:11:51 +0100

    Prototyped Message

--- src/main/java/de/lmu/ifi/sosylab/fddlj/network/Message.java
+++ src/main/java/de/lmu/ifi/sosylab/fddlj/network/Message.java
@@ -17,6 +17,11 @@ public class Message<T> {
   private String type;
 
   /**
+   * The class name of the data typed, which will be used for consistent serialization.
+   */
+  private Class<T> dataClass;
+
+  /**
    * The concrete object which should be transferred by the message.
    */
   private T data;
@@ -28,8 +33,9 @@ public class Message<T> {
    * @param type Type of the message as string.
    * @param data Data of the message
    */
-  public Message(String type, T data) {
+  public Message(String type, T data, Class<T> dataClass) {
     this.type = type;
+    this.dataClass = dataClass;
     this.data = data;
   }
 
@@ -40,6 +46,15 @@ public class Message<T> {
    */
   public String getType() {
     return type;
+  }
+
+  /**
+   * Concrete data class of the message.
+   *
+   * @return Data class of the message.
+   */
+  public Class<T> getDataClass() {
+    return dataClass;
   }
 
   /**
@@ -61,6 +76,40 @@ public class Message<T> {
     Gson gson = builder.create();
 
     return gson.toJson(this);
+  }
+
+  /**
+   * Cuts off "class " in beginning of a class name to make it usable by
+   * Class.forName().
+   *
+   * @param className Name of a class
+   * @return Sanitized name of the class
+   */
+  public static final String sanitizeToStringFromGetClass(String className) {
+    if (className.substring(0, 6).equals("class ")) {
+      className = className.substring(6);
+    }
+
+    return className;
+  }
+
+  /**
+   * Creates a {@link Message} from the given JSON string.
+   *
+   * @param json String which represented the message.
+   * @return Message from the json
+   */
+  public static Message fromJson(String json) {
+    GsonBuilder builder = new GsonBuilder();
+    Gson gson = builder.create();
+
+    System.out.println();
+    System.out.println(json);
+    System.out.println();
+
+    Message jsonParsedMessage = gson.fromJson(json, Message.class);
+
+    return jsonParsedMessage;
   }
 
 }
--- src/test/java/de/lmu/ifi/sosylab/fddlj/network/MessageTest.java
+++ src/test/java/de/lmu/ifi/sosylab/fddlj/network/MessageTest.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import de.lmu.ifi.sosylab.fddlj.model.PlayerImpl;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,7 +21,7 @@ public class MessageTest {
   @Test
   public void testGetType() {
     String type = "my-type";
-    Message message = new Message<>(type, new Object());
+    Message message = new Message<>(type, new Object(), Object.class);
 
     Assertions.assertEquals(
             type,
@@ -30,7 +32,7 @@ public class MessageTest {
   @Test
   public void testGetData() {
     Object object = new Object();
-    Message message = new Message<>("my-type", object);
+    Message message = new Message<>("my-type", object, Object.class);
 
     Assertions.assertEquals(
             object,
@@ -39,27 +41,61 @@ public class MessageTest {
   }
 
   @Test
+  public void testSanitizeToStringFromGetClass() {
+    String[][] expectedAndActual = new String[][]{
+      {"java.lang.Boolean",       "java.lang.String", "dd ava.lang.String"},
+      {"class java.lang.Boolean", "java.lang.String", "dd ava.lang.String" }
+    };
+
+    for (int i = 0; i < expectedAndActual[0].length; i++) {
+      String expected = expectedAndActual[0][i];
+      String actual = Message.sanitizeToStringFromGetClass(expectedAndActual[1][i]);
+
+      Assertions.assertEquals(
+              expected,
+              actual,
+              "sanitizeToStringFromGetClass() returned "
+                      + actual
+                      + " instead of expected " + expected);
+    }
+  }
+
+  @Test
   public void testJsonSerialization() {
 
     ArrayList<Message> messages = new ArrayList<>();
-    messages.add(new Message<>("boolean", true));
-    messages.add(new Message<>("string", "Welcome!"));
-    messages.add(new Message<>("string", new PlayerImpl("Max", new Color(0, 0, 0, 1.0))));
+    messages.add(new Message<>("boolean", true, Boolean.class));
+    messages.add(new Message<>("string", "Welcome!", String.class));
+    messages.add(new Message<>("string",
+            new PlayerImpl("Max", new Color(0, 0, 0, 1.0)), PlayerImpl.class)
+    );
 
     GsonBuilder builder = new GsonBuilder();
     Gson gson = builder.create();
 
     for (Message message : messages) {
+      // Cut of "class " in beginning
+      String dataClassName = Message.sanitizeToStringFromGetClass(
+              message.getData().getClass().toString()
+      );
+
       String jsonString =
               "{\"type\":\"" + message.getType() + "\","
+              + "\"dataClass\":\"" + dataClassName + "\","
               + "\"data\":" + gson.toJson(message.getData()) + "}";
-
-      System.out.println(jsonString);
+      String jsonMessage = message.toJson();
 
       Assertions.assertEquals(
               jsonString,
-              message.toJson(),
-              "getData() returned " + message.toJson() + " instead of expected " + jsonString);
+              jsonMessage,
+              "toJson() returned " + jsonMessage + " instead of expected " + jsonString);
+
+      Message fromJson = Message.fromJson(jsonMessage);
+
+      Assertions.assertEquals(
+              message.getData().getClass(),
+              fromJson.getData().getClass()
+      );
     }
 
   }
