61eb3e7d2ab8298d5425670078763aaaf8b202c8
Florian Theimer <f.theimer@xolf.info>
2020-01-18 22:52:24 +0100

    Fixed CustomImportOrder

--- src/main/java/de/lmu/ifi/sosylab/fddlj/network/ClientImpl.java
+++ src/main/java/de/lmu/ifi/sosylab/fddlj/network/ClientImpl.java
@@ -17,8 +17,8 @@ import de.lmu.ifi.sosylab.fddlj.network.communication.ServerNotification;
 import de.lmu.ifi.sosylab.fddlj.network.communication.Spectators;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
