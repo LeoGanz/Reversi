8f5251f3da93460f44c0732cb071588f628dd44f
Leonard Ganz <leonard.ganz@campus.lmu.de>
2020-01-26 21:25:23 +0100

    Network tests

--- src/test/java/de/lmu/ifi/sosylab/fddlj/network/NetworkTest.java
+++ src/test/java/de/lmu/ifi/sosylab/fddlj/network/NetworkTest.java
@@ -1,15 +1,21 @@
 package de.lmu.ifi.sosylab.fddlj.network;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.after;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
+import de.lmu.ifi.sosylab.fddlj.model.CellImpl;
+import de.lmu.ifi.sosylab.fddlj.model.DiskImpl;
+import de.lmu.ifi.sosylab.fddlj.model.GameField;
 import de.lmu.ifi.sosylab.fddlj.model.ModelImpl;
 import de.lmu.ifi.sosylab.fddlj.model.Player;
 import de.lmu.ifi.sosylab.fddlj.model.PlayerImpl;
 import de.lmu.ifi.sosylab.fddlj.network.communication.JoinRequest.Response;
 import de.lmu.ifi.sosylab.fddlj.network.communication.JoinRequest.Response.ResponseType;
+import de.lmu.ifi.sosylab.fddlj.network.communication.RejectedPlacement;
+import de.lmu.ifi.sosylab.fddlj.network.communication.RejectedPlacement.Reason;
 import de.lmu.ifi.sosylab.fddlj.network.communication.ServerNotification;
 import de.lmu.ifi.sosylab.fddlj.network.communication.Spectators;
 import java.io.IOException;
@@ -29,7 +35,7 @@ import org.mockito.ArgumentCaptor;
  */
 public class NetworkTest {
 
-  private static final int TIMEOUT = 1000;
+  private static final int TIMEOUT = 3000;
   private Server server;
   private final Player dummy = new PlayerImpl("Dummy", Color.BLUEVIOLET);
   private final Player dummyTwo = new PlayerImpl("DummyTwo", Color.ALICEBLUE);
@@ -97,16 +103,13 @@ public class NetworkTest {
 
   @Test
   public void testJoinAnyRandomPublicLobby_asSpectator_GameStarted() {
-    clientTwo.joinAnyRandomPublicLobby(false);
-    verifyJoinResponse(mockedGuiTwo, ResponseType.JOIN_SUCCESSFUL);
-    clientThree.joinAnyRandomPublicLobby(false);
+    letOneAndTwoJoinLobby0();
+    clientThree.joinAnyRandomPublicLobby(true);
     verifyJoinResponse(mockedGuiThree, ResponseType.JOIN_SUCCESSFUL);
-    client.joinAnyRandomPublicLobby(true);
-    verifyJoinResponse(mockedGui, ResponseType.JOIN_SUCCESSFUL);
 
-    verify(mockedGui, timeout(TIMEOUT)).receivedSpectator(any(Spectators.class));
+    verify(mockedGuiThree, timeout(TIMEOUT)).receivedSpectator(any(Spectators.class));
 
-    verify(mockedGui, timeout(TIMEOUT)).modelExchanged(any(ModelImpl.class));
+    verify(mockedGuiThree, timeout(TIMEOUT)).modelExchanged(any(ModelImpl.class));
   }
 
   @Test
@@ -149,10 +152,7 @@ public class NetworkTest {
 
   @Test
   public void testJoinLobby_NoPlayersNeeded() {
-    client.joinAnyRandomPublicLobby(false);
-    verifyJoinResponse(mockedGui, ResponseType.JOIN_SUCCESSFUL);
-    clientTwo.joinAnyRandomPublicLobby(false);
-    verifyJoinResponse(mockedGuiTwo, ResponseType.JOIN_SUCCESSFUL);
+    letOneAndTwoJoinLobby0();
     clientThree.joinSpecificLobby(false, 0);
     verifyJoinResponse(mockedGuiThree, ResponseType.NO_PLAYERS_NEEDED);
   }
@@ -172,5 +172,63 @@ public class NetworkTest {
         .receivedServerNotification(ServerNotification.SERVER_SHUTTING_DOWN);
     verify(mockedGuiTwo, timeout(TIMEOUT))
         .receivedServerNotification(ServerNotification.SERVER_SHUTTING_DOWN);
+  }
+
+  @Test
+  public void testLeavingAndResume() {
+    letOneAndTwoJoinLobby0();
+
+    client.terminate();
+    verify(mockedGuiTwo, timeout(TIMEOUT))
+        .receivedServerNotification(ServerNotification.PLAYER_ONE_LEFT);
+
+    clientThree.joinSpecificLobby(false, 0);
+    verifyJoinResponse(mockedGuiThree, ResponseType.JOIN_SUCCESSFUL);
+
+    verify(mockedGui, timeout(TIMEOUT)).modelExchanged(any(ModelImpl.class));
+    verify(mockedGuiThree, timeout(TIMEOUT)).modelExchanged(any(ModelImpl.class));
+  }
+
+  @Test
+  public void testDiskPlacement_Valid() {
+    letOneAndTwoJoinLobby0();
+
+    client.placeDisk(
+        new DiskImpl(dummy),
+        new CellImpl(GameField.STANDARD_SIZE / 2, GameField.STANDARD_SIZE / 2));
+
+    verify(mockedGui, after(TIMEOUT).never())
+        .receivedRejectedPlacementReason(any(RejectedPlacement.Reason.class));
+    verify(mockedGuiTwo, after(TIMEOUT).never())
+        .receivedRejectedPlacementReason(any(RejectedPlacement.Reason.class));
+
+  }
+
+  @Test
+  public void testDiskPlacement_NotYourTurn() {
+    letOneAndTwoJoinLobby0();
+
+    clientTwo.placeDisk(
+        new DiskImpl(dummyTwo),
+        new CellImpl(GameField.STANDARD_SIZE / 2, GameField.STANDARD_SIZE / 2));
+
+    verify(mockedGuiTwo, timeout(TIMEOUT)).receivedRejectedPlacementReason(Reason.NOT_YOUR_TURN);
+  }
+
+  @Test
+  public void testDiskPlacement_Invalid() {
+    letOneAndTwoJoinLobby0();
+
+    client.placeDisk(new DiskImpl(dummy), new CellImpl(0, 0));
+
+    verify(mockedGui, timeout(TIMEOUT)).receivedRejectedPlacementReason(Reason.INVALID_PLACEMENT);
+  }
+
+  private void letOneAndTwoJoinLobby0() {
+    client.joinAnyRandomPublicLobby(false);
+    verifyJoinResponse(mockedGui, ResponseType.JOIN_SUCCESSFUL);
+    clientTwo.joinAnyRandomPublicLobby(false);
+    verifyJoinResponse(mockedGuiTwo, ResponseType.JOIN_SUCCESSFUL);
+
   }
 }
